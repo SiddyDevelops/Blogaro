9f1d9fb496a803e342e0c61738cdbeff48c4efea
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-01-08 10:43:09 +0530

    Initial Compilation

---
+++ Blogaro/app/src/main/java/com/siddydevelops/blogaro/MainDashboard.java
@@ -0,0 +1,14 @@
+package com.siddydevelops.blogaro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+    }
+}
