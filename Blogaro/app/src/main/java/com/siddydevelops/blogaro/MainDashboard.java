e727d3292a583961c2894caa235359579040e2d6
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-01-08 14:59:12 +0530

    Added Bottom Navigation /s

--- Blogaro/app/src/main/java/com/siddydevelops/blogaro/MainDashboard.java
+++ Blogaro/app/src/main/java/com/siddydevelops/blogaro/MainDashboard.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainDashboard extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dashboard);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_main);
+        bottomNavigationView.setItemIconTintList(null);
+
     }
 }
