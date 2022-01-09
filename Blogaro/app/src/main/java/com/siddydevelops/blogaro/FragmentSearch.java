d9d771c28843d1d9dae8c208bf5c11d4bc73c431
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-01-09 11:27:17 +0530

    Added Fragments

---
+++ Blogaro/app/src/main/java/com/siddydevelops/blogaro/FragmentSearch.java
@@ -0,0 +1,23 @@
+package com.siddydevelops.blogaro;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSearch extends Fragment {
+
+    public FragmentSearch() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
--- Blogaro/app/src/main/java/com/siddydevelops/blogaro/MainDashboard.java
+++ Blogaro/app/src/main/java/com/siddydevelops/blogaro/MainDashboard.java
@@ -1,14 +1,23 @@
 package com.siddydevelops.blogaro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainDashboard extends AppCompatActivity {
+public class MainDashboard extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+
+    FragmentHome fragmentHome = new FragmentHome();
+    FragmentArticles fragmentArticles = new FragmentArticles();
+    FragmentAddBlog fragmentAddBlog = new FragmentAddBlog();
+    FragmentSearch fragmentSearch = new FragmentSearch();
+    FragmentMenu fragmentMenu = new FragmentMenu();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,33 @@ public class MainDashboard extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_navigation_main);
         bottomNavigationView.setItemIconTintList(null);
 
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentHome).commit();
+                return true;
+            case R.id.nav_articles:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentArticles).commit();
+                return true;
+            case R.id.nav_addBlog:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentAddBlog).commit();
+                return true;
+            case R.id.nav_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentSearch).commit();
+                return true;
+            case R.id.nav_menu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentMenu).commit();
+                return true;
+        }
+
+        return false;
     }
 }
