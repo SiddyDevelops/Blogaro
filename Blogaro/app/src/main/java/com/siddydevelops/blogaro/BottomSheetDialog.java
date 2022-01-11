10e049edc2017d56b6b5c9a80333b851e6549f8d
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-01-11 15:18:30 +0530

    S-BottomSheet

---
+++ Blogaro/app/src/main/java/com/siddydevelops/blogaro/BottomSheetDialog.java
@@ -0,0 +1,25 @@
+package com.siddydevelops.blogaro;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetDialog extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.profile_bottom_sheet,container,false);
+        return view;
+    }
+}
